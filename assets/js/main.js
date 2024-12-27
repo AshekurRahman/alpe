dd4a2cd220ce38fb5c8f51a549e8ba0e90955650
Ashekur Rahman <ashekurrahman1@gmail.com>
2024-12-27 11:02:08 +0600

    Off animation at mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@
 AOS.init({
   duration: 1200, // Animation duration in milliseconds
   once: true,     // Animation should happen only once
-  mobile: false,  // Disable animations on mobile devices
 });
 
 // Wrap jQuery code
